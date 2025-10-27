95ec5879fde8b46350fb726482b512d2c956d6fd
liz542 <liz@ohefsholom.org>
2025-10-27 13:00:07 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { initFirebase } from './firebase';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './styles.css'
 
-// Initialize Firebase before rendering the app
-initFirebase();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(<App />)
+JS
